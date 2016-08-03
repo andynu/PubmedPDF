9bcc7d16c633d37116ff1985f6c345ae989622e6
Andy <andy@wi.mit.edu>
2016-08-03 12:41:16 -0400

    Remove rdoc/usage.
    Add Gemfile for dependencies.
    Add default log directory.

--- script/pubmedid2pdf.rb
+++ script/pubmedid2pdf.rb
@@ -1,34 +1,36 @@
 #!/usr/bin/env ruby 
 
-# == Synopsis 
-#   This program tries to download a PDF file for the given comma-separated pubmed IDs
-#
-# == Required GEMS
-#     mechanize (tested with 0.9.3)
-#     socksify (tested with 1.1.0) (if you plan on using SOCKS)
-#     progressbar
-#
-# == Examples
-#     pubmedid2pdf.rb 19508715,18677110,19450510,19450585
-#
-#   Other examples:
-#    This example downloads through SOCKS, here we are using a localhost connection through port 9999
-#    Meaning that you can ssh to your some server you have access to that can access some PDFs that you cannot, f.ex. your University
-#    This is done with this co  mmand: ssh -D 9999 username@server in another terminal
-#    To use SOCKS call the program with the server and the port, in this case 127.0.0.1 and 9999
-#     pubmedid2pdf.rb 19508715 127.0.0.1 9999
-#
-# == Usage 
-#    pubmedid2pdf.rb pubmedid/s [server] [port]
-#
-# == Author
-#   Bio-geeks (adapted a script by Edoardo "Dado" Marcora, Ph.D.)
-#   <http://bio-geeks.com>
-#
-# == Copyright
-#   Copyright (c) 2009-2010 Bio-geeks. Licensed under the MIT License:
-#   http://www.opensource.org/licenses/mit-license.php
-require 'rdoc/usage'
+USAGE = <<DOC
+== Synopsis 
+  This program tries to download a PDF file for the given comma-separated pubmed IDs
+
+== Required GEMS
+    mechanize (tested with 0.9.3)
+    socksify (tested with 1.1.0) (if you plan on using SOCKS)
+    progressbar
+
+== Examples
+    pubmedid2pdf.rb 19508715,18677110,19450510,19450585
+
+  Other examples:
+   This example downloads through SOCKS, here we are using a localhost connection through port 9999
+   Meaning that you can ssh to your some server you have access to that can access some PDFs that you cannot, f.ex. your University
+   This is done with this co  mmand: ssh -D 9999 username@server in another terminal
+   To use SOCKS call the program with the server and the port, in this case 127.0.0.1 and 9999
+    pubmedid2pdf.rb 19508715 127.0.0.1 9999
+
+== Usage 
+   pubmedid2pdf.rb pubmedid/s [server] [port]
+
+== Author
+  Bio-geeks (adapted a script by Edoardo "Dado" Marcora, Ph.D.)
+  <http://bio-geeks.com>
+
+== Copyright
+  Copyright (c) 2009-2010 Bio-geeks. Licensed under the MIT License:
+  http://www.opensource.org/licenses/mit-license.php
+DOC
+
 require 'rubygems'
 require 'progressbar'
 require File.join(File.dirname(__FILE__), '..', 'app','pdfetch')
@@ -43,7 +45,8 @@ save_dir = Dir.pwd + "/pdf/"
 
 pubmeds_array = Array.new
 if (pubmeds.nil?)
-  RDoc::usage() #exits app
+  puts USAGE
+  exit
 end
 if (File.exists?(pubmeds)) # read pubmed ids from file, one per line
   pubmeds_array = IO.readlines(pubmeds).map{|l| l.chomp}
--- script/searchTerm2pdf.rb
+++ script/searchTerm2pdf.rb
@@ -1,47 +1,49 @@
 #!/usr/bin/env ruby 
 
-# == Synopsis 
-#   This program tries to download a PDF file for the given search term
-#
-# == Required GEMS
-#     mechanize (0.9.3)
-#     bio (1.3.0)
-#     socksify (1.1.0) (if you plan on using SOCKS)
-#
-# == Examples
-#     searchTerm2pdf.rb 'Torarinsson E[Author]' > list
-#     NB! The output list is just like a .csv file except that we use '|' and not ',' as a separator
-#
-#   Other examples:
-#    This example downloads through SOCKS, here we are using a localhost connection through port 9999
-#    Meaning that you can ssh to your some server you have access to that can access some PDFs that you cannot, f.ex. your University
-#    This is done with this command: ssh -D 9999 username@server in another terminal
-#    To use SOCKS call the program with the server and the port, in this case 127.0.0.1 and 9999
-#     searchTerm2pdf.rb 'Torarinsson E[Author]' 127.0.0.1 9999
-#
-# == Usage 
-#    searchTerm2pdf.rb search-term [server] [port] > list
-#
-# == Author
-#   Bio-geeks (adapted a script by Edoardo "Dado" Marcora, Ph.D.)
-#   <http://bio-geeks.com>
-#
-# == Copyright
-#   Copyright (c) 2009 Bio-geeks. Licensed under the MIT License:
-#   http://www.opensource.org/licenses/mit-license.php
+USAGE = <<DOC
+== Synopsis 
+  This program tries to download a PDF file for the given search term
+
+== Required GEMS
+    mechanize (0.9.3)
+    bio (1.3.0)
+    socksify (1.1.0) (if you plan on using SOCKS)
+
+== Examples
+    searchTerm2pdf.rb 'Torarinsson E[Author]' > list
+    NB! The output list is just like a .csv file except that we use '|' and not ',' as a separator
+
+  Other examples:
+   This example downloads through SOCKS, here we are using a localhost connection through port 9999
+   Meaning that you can ssh to your some server you have access to that can access some PDFs that you cannot, f.ex. your University
+   This is done with this command: ssh -D 9999 username@server in another terminal
+   To use SOCKS call the program with the server and the port, in this case 127.0.0.1 and 9999
+    searchTerm2pdf.rb 'Torarinsson E[Author]' 127.0.0.1 9999
+
+== Usage 
+   searchTerm2pdf.rb search-term [server] [port] > list
+
+== Author
+  Bio-geeks (adapted a script by Edoardo "Dado" Marcora, Ph.D.)
+  <http://bio-geeks.com>
+
+== Copyright
+  Copyright (c) 2009 Bio-geeks. Licensed under the MIT License:
+  http://www.opensource.org/licenses/mit-license.php
+DOC
 
 require 'rubygems'
 require 'bio'
 require 'date'
-require 'pdfetch.rb'
-require 'rdoc/usage'
+require File.join(File.dirname(__FILE__), '..', 'app','pdfetch')
 
 #query = '(Lindow M[Author] OR Torarinsson E[Author] OR Lindgreen S[Author] OR Marstrand T[Author]) AND (1998/01/01[PDAT]:3000[PDAT])'
 #query = "Torarinsson"
 query = ARGV[0]
 
 if (query.nil?)
-  RDoc::usage() #exits app
+  puts USAGE
+  exit
 end
 
 ids = Bio::PubMed.esearch(query, {'retmax' => 5000})
@@ -53,7 +55,7 @@ else
   warn "Couldn't find any articles matching your search term"  
 end
 
-fetcher = Fetch.new()
+fetcher = Pdfetch::Fetcher.new()
 server = ARGV[1]
 port = ARGV[2]
 if (!server.nil? && !port.nil?)
